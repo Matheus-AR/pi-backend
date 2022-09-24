8829bb7cdd6995d9493486549934435c4665b249
Matheus-AR <matheusaraujorodrigues@outlook.com>
2022-09-24 09:14:49 -0300

    Adicionando o swagger no projeto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const anunciosRoutes = require('./routes/anuncios');
 const usuariosRoutes = require('./routes/usuarios')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/anuncios', anunciosRoutes);
 app.use('/usuarios', usuariosRoutes);
